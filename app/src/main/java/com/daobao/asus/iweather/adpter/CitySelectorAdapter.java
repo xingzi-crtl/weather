a304bec49acebd7e9084b7bba0a415eaf0ea14ae
xingzi--crtl <2399320104@qq.com>
2020-12-04 08:04:34 +0800

    Initial commit

---
+++ app/src/main/java/com/daobao/asus/iweather/adpter/CitySelectorAdapter.java
@@ -0,0 +1,21 @@
+package com.daobao.asus.iweather.adpter;
+
+import android.support.annotation.Nullable;
+
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.chad.library.adapter.base.BaseViewHolder;
+import com.daobao.asus.iweather.R;
+
+import java.util.List;
+
+public class CitySelectorAdapter extends BaseQuickAdapter<String,BaseViewHolder>{
+
+    public CitySelectorAdapter(int layoutResId, @Nullable List<String> data) {
+        super(layoutResId, data);
+    }
+
+    @Override
+    protected void convert(BaseViewHolder helper, String item) {
+        helper.setText(R.id.item_select_cityName,item);
+    }
+}
